3aaf2c3ea9662494a713a4acc47d9fb4acacc6cf
Ilya Shiyan <ilya.shiyan97@mail.ru>
2022-11-12 11:15:31 +0300

    Finished project

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte a = 2;
+        short b = 3;
+        int c = 5;
+
+        Byte byte1 = Byte.valueOf(a);
+        Short short1 = Short.valueOf(b);
+        Integer integer1 = Integer.valueOf(c);
+
+    }
+}
